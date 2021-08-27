8c47e1dbcd7e6b24c3f1ac60ee6e7ed43dc36a72
Marq Gray <marquis.s.sandifer@gmail.com>
2021-08-26 19:08:50 -0500

    Added play and pause functionality

--- script.ts
+++ script.ts
@@ -17,11 +17,44 @@ const app = new (class {
 
   constructor() {
     this.loadSong(this.songs[this.songIndex]);
+    this.htmlElements.playButton.addEventListener("click", () => {
+      const isPlaying =
+        this.htmlElements.musicContainer.classList.contains("play");
+      if (isPlaying) {
+        this.pauseSong();
+      } else {
+        this.playSong();
+      }
+    });
   }
 
   loadSong(song: string) {
     this.htmlElements.title.innerText = song;
     this.htmlElements.audio.src = `music/${song}.mp3`;
     this.htmlElements.cover.src = `images/${song}.jpg`;
+  }
+
+  playSong() {
+    this.htmlElements.musicContainer.classList.add("play");
+    this.htmlElements.playButton
+      .querySelector("i.fas")
+      .classList.remove("fa-play");
+    this.htmlElements.playButton
+      .querySelector("i.fas")
+      .classList.add("fa-pause");
+
+    this.htmlElements.audio.play();
+  }
+
+  pauseSong() {
+    this.htmlElements.musicContainer.classList.remove("play");
+    this.htmlElements.playButton
+      .querySelector("i.fas")
+      .classList.add("fa-play");
+    this.htmlElements.playButton
+      .querySelector("i.fas")
+      .classList.remove("fa-pause");
+
+    this.htmlElements.audio.pause();
   }
 })();
